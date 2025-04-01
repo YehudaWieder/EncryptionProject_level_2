7be7d5935948e99ebf4a8af9dbf9c13166b59f34
YehudaWieder <v4105677@gmail.com>
2025-04-01 16:57:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyinputplus as pyip
 
 def main():
     # Import functions for handling different tasks
-    from input_functions import encrypting_input, decrypting_input, steganography_input, steganography_extraction_input
+    from input_functions import encrypting_input, decrypting_input
 
     while True:
         # Display the main menu with options
@@ -10,20 +10,16 @@ def main():
         What would you like to do?
         1. Encrypt a text file
         2. Decrypt a text file
-        3. Hide a message in an image
-        4. Extract a message from an image
-        5. Exit""")
+        3. Exit""")
 
         # Get the user's input, ensuring it's an integer between 1 and 5
-        response = pyip.inputInt("Please enter your choice as a num 1 - 5: \n", min=1, max=5)
+        response = pyip.inputInt("Please enter your choice as a num 1 - 3: \n", min=1, max=3)
 
         # Mapping user input to corresponding functions or actions
         menus = {
             1: encrypting_input,  # Encrypt a text file
             2: decrypting_input,  # Decrypt a text file
-            3: steganography_input,  # Hide a message in an image
-            4: steganography_extraction_input,  # Extract a message from an image
-            5: exit  # Exit the program
+            3: exit  # Exit the program
         }
 
         # Execute the corresponding function based on user's choice
